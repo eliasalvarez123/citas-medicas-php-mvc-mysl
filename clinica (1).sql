ba1f9832d3662577d26c98138b9d63437c104fb4
eliasalvarez123 <ingenieroeliasalvarez@hotmail.com>
2020-05-14 12:32:27 -0500

    version 1

---
+++ clinica (1).sql
@@ -0,0 +1,278 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-05-2020 a las 18:53:08
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `clinica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administradores`
+--
+
+CREATE TABLE `administradores` (
+  `id` int(11) NOT NULL,
+  `usuario` text COLLATE utf8_spanish_ci NOT NULL,
+  `clave` text COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` text COLLATE utf8_spanish_ci NOT NULL,
+  `apellido` text COLLATE utf8_spanish_ci NOT NULL,
+  `foto` text COLLATE utf8_spanish_ci NOT NULL,
+  `rol` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `administradores`
+--
+
+INSERT INTO `administradores` (`id`, `usuario`, `clave`, `nombre`, `apellido`, `foto`, `rol`) VALUES
+(1, 'admin', '123', 'Alejandro', 'Petrelli', '', 'Administrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `id` int(11) NOT NULL,
+  `id_doctor` int(11) NOT NULL,
+  `id_consultorio` int(11) NOT NULL,
+  `id_paciente` int(11) NOT NULL,
+  `documento` text COLLATE utf8_spanish2_ci NOT NULL,
+  `inicio` datetime NOT NULL,
+  `fin` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consultorios`
+--
+
+CREATE TABLE `consultorios` (
+  `id` int(11) NOT NULL,
+  `nombre` text COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `consultorios`
+--
+
+INSERT INTO `consultorios` (`id`, `nombre`) VALUES
+(1, 'Cardiologia'),
+(2, 'Psicología'),
+(3, 'Quimioterapia ');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `doctores`
+--
+
+CREATE TABLE `doctores` (
+  `id` int(11) NOT NULL,
+  `id_consultorio` int(11) NOT NULL,
+  `apellido` text COLLATE utf8_spanish2_ci NOT NULL,
+  `nombre` text COLLATE utf8_spanish2_ci NOT NULL,
+  `foto` text COLLATE utf8_spanish2_ci NOT NULL,
+  `usuario` text COLLATE utf8_spanish2_ci NOT NULL,
+  `clave` text COLLATE utf8_spanish2_ci NOT NULL,
+  `sexo` text COLLATE utf8_spanish2_ci NOT NULL,
+  `horarioE` time NOT NULL,
+  `horarioS` time NOT NULL,
+  `rol` text COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `doctores`
+--
+
+INSERT INTO `doctores` (`id`, `id_consultorio`, `apellido`, `nombre`, `foto`, `usuario`, `clave`, `sexo`, `horarioE`, `horarioS`, `rol`) VALUES
+(10, 1, 'alvarez', 'carlos', '', 'carlos', '123', 'Masculino', '00:00:00', '00:00:00', 'Doctor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inicio`
+--
+
+CREATE TABLE `inicio` (
+  `id` int(11) NOT NULL,
+  `intro` text COLLATE utf8_spanish_ci NOT NULL,
+  `horaE` time NOT NULL,
+  `horaS` time NOT NULL,
+  `direccion` text COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` text COLLATE utf8_spanish_ci NOT NULL,
+  `correo` text COLLATE utf8_spanish_ci NOT NULL,
+  `logo` text COLLATE utf8_spanish_ci NOT NULL,
+  `favicon` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `inicio`
+--
+
+INSERT INTO `inicio` (`id`, `intro`, `horaE`, `horaS`, `direccion`, `telefono`, `correo`, `logo`, `favicon`) VALUES
+(1, 'Clínica Médica', '09:00:00', '20:00:00', 'Calle 4412', '5155665', 'admin@hotmail.com', 'Vistas/img/logo.png', 'Vistas/img/favicon.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `id` int(11) NOT NULL,
+  `nombre` text COLLATE utf8_spanish2_ci NOT NULL,
+  `apellido` text COLLATE utf8_spanish2_ci NOT NULL,
+  `documento` text COLLATE utf8_spanish2_ci NOT NULL,
+  `usuario` text COLLATE utf8_spanish2_ci NOT NULL,
+  `clave` text COLLATE utf8_spanish2_ci NOT NULL,
+  `rol` text COLLATE utf8_spanish2_ci NOT NULL,
+  `foto` text COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `pacientes`
+--
+
+INSERT INTO `pacientes` (`id`, `nombre`, `apellido`, `documento`, `usuario`, `clave`, `rol`, `foto`) VALUES
+(2, 'elias', 'alvarez', '5470883', 'elias', '5470883', 'Paciente', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `secretarias`
+--
+
+CREATE TABLE `secretarias` (
+  `id` int(11) NOT NULL,
+  `usuario` text COLLATE utf8_spanish2_ci NOT NULL,
+  `clave` text COLLATE utf8_spanish2_ci NOT NULL,
+  `nombre` text COLLATE utf8_spanish2_ci NOT NULL,
+  `apellido` text COLLATE utf8_spanish2_ci NOT NULL,
+  `foto` text COLLATE utf8_spanish2_ci NOT NULL,
+  `rol` text COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `secretarias`
+--
+
+INSERT INTO `secretarias` (`id`, `usuario`, `clave`, `nombre`, `apellido`, `foto`, `rol`) VALUES
+(2, 'mary', '123', 'mary', 'alvarez', '', 'Secretaria');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administradores`
+--
+ALTER TABLE `administradores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `consultorios`
+--
+ALTER TABLE `consultorios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `doctores`
+--
+ALTER TABLE `doctores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `inicio`
+--
+ALTER TABLE `inicio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `secretarias`
+--
+ALTER TABLE `secretarias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administradores`
+--
+ALTER TABLE `administradores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `consultorios`
+--
+ALTER TABLE `consultorios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `doctores`
+--
+ALTER TABLE `doctores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `inicio`
+--
+ALTER TABLE `inicio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `secretarias`
+--
+ALTER TABLE `secretarias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
